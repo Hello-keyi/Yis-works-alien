bde826a60b023e8ac336f8730620b33bc1e408ba
yi <13692959211.com>
2026-06-20 21:28:57 +0800

    这里有主循环2026.6.20

---
+++ alien_invasion.py.py
@@ -0,0 +1,22 @@
+import sys
+import pygame
+
+class AlienInvasion():
+    def __init__(self):
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode((1200,800))
+        self.bg_color = (230,230,230)
+
+    def run_game(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT():
+                    sys.exit()
+            
+            pygame.display.flip()
+            self.clock.tick(60)
+
+if __name__ == "_main_":
+    ai = AlienInvasion()
+    ai.run_game()
